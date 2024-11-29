b66e45ba8c1dfd5abf82c817595c4a66467c070f
lbh666 <1050208869@qq.com>
2024-11-30 00:30:35 +0800

    trace back

--- params.py
+++ params.py
@@ -11,16 +11,14 @@ class GaussianParams(nn.Module):
         means: torch.Tensor,
         quats: torch.Tensor,
         scales: torch.Tensor,
-        features_dc: torch.Tensor,
-        features_rest: torch.Tensor,
+        colors: torch.Tensor,
         opacities: torch.Tensor,
         scene_center: torch.Tensor | None = None,
         scene_scale: torch.Tensor | float = 1.0,
-        sh_degree: int = 3
     ):
         super().__init__()
         if not check_gaussian_sizes(
-            means, quats, scales, features_dc, features_rest, opacities
+            means, quats, scales, colors, opacities
         ):
             import ipdb
 
@@ -29,16 +27,15 @@ class GaussianParams(nn.Module):
             "means": nn.Parameter(means),
             "quats": nn.Parameter(quats),
             "scales": nn.Parameter(scales),
-            "features_dc": nn.Parameter(features_dc),
-            "features_rest": nn.Parameter(features_rest),
+            "colors": nn.Parameter(colors),
             "opacities": nn.Parameter(opacities),
         }
         self.params = nn.ParameterDict(params_dict)
         self.quat_activation = lambda x: F.normalize(x, dim=-1, p=2)
+        self.color_activation = torch.sigmoid
         self.scale_activation = torch.exp
         self.opacity_activation = torch.sigmoid
         self.motion_coef_activation = lambda x: F.softmax(x, dim=-1)
-        self.sh_degree = sh_degree
 
         if scene_center is None:
             scene_center = torch.zeros(3, device=means.device)
@@ -47,10 +44,9 @@ class GaussianParams(nn.Module):
 
     @staticmethod
     def init_from_state_dict(state_dict, prefix="params."):
-        req_keys = ["means", "quats", "scales", "feature_dc", "feature_rest", "opacities"]
+        req_keys = ["means", "quats", "scales", "colors", "opacities"]
         assert all(f"{prefix}{k}" in state_dict for k in req_keys)
         args = {
-            "motion_coefs": None,
             "scene_center": torch.zeros(3),
             "scene_scale": torch.tensor(1.0),
         }
@@ -62,21 +58,9 @@ class GaussianParams(nn.Module):
     @property
     def num_gaussians(self) -> int:
         return self.params["means"].shape[0]
-    
-    @property
-    def colors(self) -> torch.Tensor:
-        if self.config.sh_degree > 0:
-            return SH2RGB(self.params['feature_dc'])
-        else:
-            return torch.sigmoid(self.params['feature_dc'])
-        
-    @property
-    def features_dc(self) -> torch.Tensor:
-        return self.params["features_dc"]
 
-    @property
-    def features_rest(self) -> torch.Tensor:
-        return self.params["features_rest"]
+    def get_colors(self) -> torch.Tensor:
+        return self.color_activation(self.params["colors"])
 
     def get_scales(self) -> torch.Tensor:
         return self.scale_activation(self.params["scales"])
@@ -125,38 +109,26 @@ class GaussianParams(nn.Module):
         updated_params = {"opacities": self.params["opacities"]}
         return updated_params
 
-def num_sh_bases(degree: int) -> int:
-    """
-    Returns the number of spherical harmonic bases for a given degree.
-    """
-    assert degree <= 4, "We don't support degree greater than 4."
-    return (degree + 1) ** 2
 
 def check_gaussian_sizes(
     means: torch.Tensor,
     quats: torch.Tensor,
     scales: torch.Tensor,
-    feature_dc: torch.Tensor,
-    feature_rest: torch.Tensor,
+    colors: torch.Tensor,
     opacities: torch.Tensor,
-    sh_degree: int = 3,
 ) -> bool:
-    dim_sh = num_sh_bases(sh_degree)
     dims = means.shape[:-1]
     leading_dims_match = (
         quats.shape[:-1] == dims
         and scales.shape[:-1] == dims
-        and feature_dc.shape[:-1] == dims
-        and feature_rest.shape[:-2] == dims
+        and colors.shape[:-1] == dims
         and opacities.shape == dims
     )
     dims_correct = (
         means.shape[-1] == 3
         and (quats.shape[-1] == 4)
         and (scales.shape[-1] == 3)
-        and (feature_dc.shape[-1] == 3)
-        and (feature_rest.shape[-1] == 3)
-        and (feature_rest.shape[-2] == dim_sh)
+        and (colors.shape[-1] == 3)
     )
     return leading_dims_match and dims_correct
 
@@ -167,18 +139,3 @@ def check_bases_sizes(motion_rots: torch.Tensor, motion_transls: torch.Tensor) -
         and motion_transls.shape[-1] == 3
         and motion_rots.shape[:-2] == motion_transls.shape[:-2]
     )
-
-def RGB2SH(rgb):
-    """
-    Converts from RGB values [0,1] to the 0th spherical harmonic coefficient
-    """
-    C0 = 0.28209479177387814
-    return (rgb - 0.5) / C0
-
-
-def SH2RGB(sh):
-    """
-    Converts from the 0th spherical harmonic coefficient to RGB values [0,1]
-    """
-    C0 = 0.28209479177387814
-    return sh * C0 + 0.5
